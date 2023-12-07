3c27b9835d156a256644f257c1b4de27bdcffe65
Mikhail Ryzhov <rymiser@gmail.com>
2023-12-07 18:22:49 +0100

    go fmt

--- app/main.go
+++ app/main.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-    "github.com/webview/webview_go"
+	"flag"
+	"fmt"
 	"github.com/rymis/leo-learns-prog/learnsrv/server"
-    "fmt"
-    "os"
-    "flag"
+	"github.com/webview/webview_go"
+	"net"
 	"net/http"
-    "net"
+	"os"
 )
 
 func main() {
 	dataPath := flag.String("data", "./data", "specify directory with user data")
 	wwwPath := flag.String("wwwdata", "../programming-tasks/dist", "path to static WWW data")
-    debug := flag.Bool("debug", false, "Enable debug mode")
+	debug := flag.Bool("debug", false, "Enable debug mode")
 
-    flag.Parse()
+	flag.Parse()
 
 	srv, err := server.New(*dataPath, *wwwPath)
 	if err != nil {
@@ -27,24 +27,24 @@ func main() {
 		Handler: srv,
 	}
 
-    listener, err := net.Listen("tcp", ":0")
-    if err != nil {
+	listener, err := net.Listen("tcp", ":0")
+	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 		os.Exit(1)
-    }
+	}
 
-    port := listener.Addr().(*net.TCPAddr).Port
-    addr := fmt.Sprintf("http://localhost:%d", port)
+	port := listener.Addr().(*net.TCPAddr).Port
+	addr := fmt.Sprintf("http://localhost:%d", port)
 
-    go func () {
-        s.Serve(listener)
-    }()
+	go func() {
+		s.Serve(listener)
+	}()
 
-    app := webview.New(*debug)
-    defer app.Destroy()
+	app := webview.New(*debug)
+	defer app.Destroy()
 
-    app.SetTitle("Leo learns programming")
-    app.Navigate(addr)
-    app.SetSize(800, 600, webview.HintNone)
-    app.Run()
+	app.SetTitle("Leo learns programming")
+	app.Navigate(addr)
+	app.SetSize(800, 600, webview.HintNone)
+	app.Run()
 }
